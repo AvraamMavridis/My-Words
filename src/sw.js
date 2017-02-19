da5898220e488e0f01d0829fbad4f12ab4f32d21
AvraamMavridis <avraammauridis@gmail.com>
2017-02-19 10:03:23 +0100

    error handling in sw

--- src/sw.js
+++ src/sw.js
@@ -1,23 +1,28 @@
 self.addEventListener('install', function(event) {
   // pre cache a load of stuff:
   event.waitUntil(
-    caches.open('mycache').then(function(cache) {
+    caches.open('mycache')
+      .then(function(cache) {
       return cache.addAll([
-        '/',
-        '/dist/',
-        '/dist/bundle.js',
-        '/dist/app.css',
-        '/dist/index.html',
-        'https://cdnjs.cloudflare.com/ajax/libs/antd/2.7.2/antd.css'
-      ]);
+        '/'
+      ])
+      .catch(function(error){
+        console.error('Error on installing');
+        console.error(error);
+      });
     })
   )
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request)
+      .then(function(response) {
       return response || fetch(event.request);
+    })
+    .catch(function(error){
+      console.error('Error on fetching');
+      console.error(error);
     })
   );
 });
